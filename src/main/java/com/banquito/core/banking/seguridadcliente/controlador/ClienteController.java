a6774821f5938735756bf5e1b3742976415ed844
bpbryalex98 <bpbryalex98@gmail.com>
2024-01-01 02:40:49 -0500

    Servicios seguridad completos

--- src/main/java/com/banquito/core/banking/seguridadcliente/controlador/ClienteController.java
+++ src/main/java/com/banquito/core/banking/seguridadcliente/controlador/ClienteController.java
@@ -1,21 +1,24 @@
 package com.banquito.core.banking.seguridadcliente.controlador;
 
+import com.banquito.core.banking.seguridadcliente.domain.Cliente;
+import com.banquito.core.banking.seguridadcliente.services.ClienteService;
+
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.banquito.core.banking.seguridadcliente.domain.Cliente;
-import com.banquito.core.banking.seguridadcliente.services.ClienteService;
 
-import jakarta.transaction.Transactional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/seg-cliente")
 public class ClienteController {
@@ -33,7 +36,6 @@ public class ClienteController {
         return clienteService.auntenticar(usuario, contrasena);
     }
 
-    @Transactional
     @PostMapping("/crear")
     public ResponseEntity<Cliente> crearCliente(@RequestBody Cliente nuevoCliente) {
         Cliente clienteCreado = clienteService.crearCliente(nuevoCliente);
@@ -54,6 +56,12 @@ public class ClienteController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PutMapping("/actualizar-mfa/{idCliente}/{nuevoValorMFA}")
+    public ResponseEntity<String> actualizarMFA(@PathVariable Integer idCliente, @PathVariable String nuevoValorMFA) {
+        clienteService.actualizarAtributosCliente(idCliente, nuevoValorMFA);
+        return new ResponseEntity<>("MFA actualizado correctamente", HttpStatus.OK);
     }
 
 
--- src/main/java/com/banquito/core/banking/seguridadcliente/services/ClienteService.java
+++ src/main/java/com/banquito/core/banking/seguridadcliente/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.banquito.core.banking.seguridadcliente.services;
 import com.banquito.core.banking.seguridadcliente.dao.ClienteRepository;
 import com.banquito.core.banking.seguridadcliente.domain.Cliente;
 
+import jakarta.transaction.Transactional;
+
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -20,11 +22,24 @@ public class ClienteService {
         return clienteRepository.findByUsuarioAndContrasena(usuario, contrasena);
     }
 
+    @Transactional
     public Cliente crearCliente(Cliente nuevoCliente) {
         return clienteRepository.save(nuevoCliente);
     }
 
     public Optional<Cliente> bucarPorId(Integer codCliente){
         return clienteRepository.findById(codCliente);
+    }
+
+    @Transactional
+    public void actualizarAtributosCliente(Integer idCliente, String codigoMfa) {
+        Optional<Cliente> clienteOptional = clienteRepository.findById(idCliente);
+
+        if (clienteOptional.isPresent()) {
+            Cliente cliente = clienteOptional.get();
+            cliente.setMfa(codigoMfa);
+
+            clienteRepository.save(cliente); 
+        }
     }
 }
