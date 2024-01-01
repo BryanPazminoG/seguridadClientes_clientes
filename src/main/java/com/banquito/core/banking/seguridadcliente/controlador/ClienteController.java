0130f69519c2ef9eb62235c6debf7faf4b0fb1d1
bpbryalex98 <bpbryalex98@gmail.com>
2023-12-31 19:37:29 -0500

    Servicios Seguridad clientes

--- src/main/java/com/banquito/core/banking/seguridadcliente/controlador/ClienteController.java
+++ src/main/java/com/banquito/core/banking/seguridadcliente/controlador/ClienteController.java
@@ -1,10 +1,60 @@
 package com.banquito.core.banking.seguridadcliente.controlador;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.banquito.core.banking.seguridadcliente.domain.Cliente;
+import com.banquito.core.banking.seguridadcliente.services.ClienteService;
+
+import jakarta.transaction.Transactional;
+
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/seg-cliente")
 public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping("/autenticar")
+    public Cliente autenticarCliente(@RequestBody Cliente usuarioContrasena) {
+        String usuario = usuarioContrasena.getUsuario();
+        String contrasena = usuarioContrasena.getContrasena();
+        return clienteService.auntenticar(usuario, contrasena);
+    }
+
+    @Transactional
+    @PostMapping("/crear")
+    public ResponseEntity<Cliente> crearCliente(@RequestBody Cliente nuevoCliente) {
+        Cliente clienteCreado = clienteService.crearCliente(nuevoCliente);
+
+        if (clienteCreado != null) {
+            return new ResponseEntity<>(clienteCreado, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/clientes/{codCliente}")
+    public ResponseEntity<Cliente> buscarClientePorId(@PathVariable Integer codCliente) {
+        Optional<Cliente> clienteEncontrado = clienteService.bucarPorId(codCliente);
+
+        if (clienteEncontrado.isPresent()) {
+            return new ResponseEntity<>(clienteEncontrado.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 
 }
--- src/main/java/com/banquito/core/banking/seguridadcliente/services/ClienteService.java
+++ src/main/java/com/banquito/core/banking/seguridadcliente/services/ClienteService.java
@@ -2,9 +2,12 @@ package com.banquito.core.banking.seguridadcliente.services;
 
 import com.banquito.core.banking.seguridadcliente.dao.ClienteRepository;
 import com.banquito.core.banking.seguridadcliente.domain.Cliente;
-import org.springframework.stereotype.Service;
-@Service
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClienteService {
 
     private ClienteRepository clienteRepository;
@@ -13,7 +16,15 @@ public class ClienteService {
         this.clienteRepository = clienteRepository;
     }
 
-    public Cliente authenticateCliente(String usuario, String contrasena) {
+    public Cliente auntenticar(String usuario, String contrasena) {
         return clienteRepository.findByUsuarioAndContrasena(usuario, contrasena);
+    }
+
+    public Cliente crearCliente(Cliente nuevoCliente) {
+        return clienteRepository.save(nuevoCliente);
+    }
+
+    public Optional<Cliente> bucarPorId(Integer codCliente){
+        return clienteRepository.findById(codCliente);
     }
 }
