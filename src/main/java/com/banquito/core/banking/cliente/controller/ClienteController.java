86999be601aa5786ddf759ca4782107ea5e5d0b1
RUBEN ALFREDO TOMALA VERA <ruben.al97@hotmail.com>
2024-02-12 10:28:38 -0500

    cors

--- src/main/java/com/banquito/core/banking/cliente/controller/ClienteController.java
+++ src/main/java/com/banquito/core/banking/cliente/controller/ClienteController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +15,7 @@ import com.banquito.core.banking.cliente.domain.Cliente;
 import com.banquito.core.banking.cliente.dto.ClienteDTO;
 import com.banquito.core.banking.cliente.service.ClienteService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Slf4j
