91a04e1bb4a002e635b7a8b30c282124bd853b24
RUBEN ALFREDO TOMALA VERA <ruben.al97@hotmail.com>
2024-03-16 12:08:36 -0500

    Cambio FIelds

--- src/main/java/com/banquito/core/banking/cliente/domain/CodigoVerificacion.java
+++ src/main/java/com/banquito/core/banking/cliente/domain/CodigoVerificacion.java
@@ -2,6 +2,8 @@ package com.banquito.core.banking.cliente.domain;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +13,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CodigoVerificacion {
   private String codigo;
+  @Field("fecha_creacion")
   private LocalDateTime fechaCreacion;
   private String estado;
 }
